20f11786d42e4042d9363d9c80a824e50c2c2a71
Proteco <proteco@Mac-mini-de-Proteco.local>
2020-05-27 18:21:52 -0500

    funciones

--- ejemplos/funciones.js
+++ ejemplos/funciones.js
@@ -1,10 +1,36 @@
 // Creacion de funciones por medio de expresiones 
-let saludar = function(nombre){
+const saludar = function(nombre){
     console.log("Hola, cómo estas? "+nombre)
 }
 
-let saludar = function(nombre){
-    console.log("Hackieado")
+saludar("Elizabeth")
+
+
+// Anidamiento de funciones
+function addSquares(a,b){
+    function square(x){
+        return x*x
+    }   
+
+    return square(a) + square(b)
 }
 
-saludar("Elizabeth")
+console.log(addSquares(2,3))
+
+//Funciones callback
+// Son funciones que llaman a otras funciones
+function map(arreglo,funcion){
+    var resultado = []
+    for(var i= 0; i<arreglo.length; i++){
+        resultado.push(funcion(arreglo[i])) // 3 -> 27
+    }  
+    return resultado  
+}    
+
+//let result = map(function(num){return num*num*num},[1,2,3,4])
+//console.log(result)
+
+let result = map([1,2,3,4],num => {
+    return num*num*num
+})
+console.log(result)
