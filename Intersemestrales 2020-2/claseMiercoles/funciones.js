ba61c8d93993e6c64c35d83d36da9e7496dfca17
rhofp <rhodfra@gmail.com>
2020-08-12 15:28:30 -0500

    Added arrow functions xpl

--- Intersemestrales 2020-2/claseMiercoles/funciones.js
+++ Intersemestrales 2020-2/claseMiercoles/funciones.js
@@ -19,3 +19,39 @@ console.log(saludoMarker('Rodrigo'))
 ;(function () {
   console.log('Me ejecuto y me destruyo inmediatamente')
 })()
+
+// Arrow functions (ES6)
+
+// const multiplicacion = (num1, num2) => {
+//   return num1 * num2
+// }
+
+// Se puede quitar return y {} porque solo tenemos una linea de codigo en la fn
+const multiplicacion = (num1, num2) => num1 * num2
+const mostrarNumeros = (num1, num2) => console.log(num1, num2)
+
+console.log(multiplicacion(4, 2))
+mostrarNumeros(3, 1)
+
+// Ejemplo de funcin sin parametros
+const saludo = () => console.log('Hola mundo')
+saludo()
+
+const saludarPersona = (nombre) => console.log('Hola mundo ' + nombre)
+saludarPersona('Armando')
+;(() => {
+  console.log('Me ejecuto y me destruyo inmediatamente')
+})()
+;(() => console.log('HOLAAAA'))()
+
+// {nombre:'Gabriel',edad:22}
+const nombre = 'Gabriel'
+const edad = 22
+
+// const objPersonaMaker = (nombre, edad) => {
+//   return { nombre: nombre, edad: edad }
+// }
+
+const objPersonaMaker = (nombre, edad) => ({ nombre: nombre, edad: edad })
+const persona = objPersonaMaker(nombre, edad)
+console.log(persona)
