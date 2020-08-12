117f95174fb7225d6c7a853c786396563e86a0b5
rhofp <rhodfra@gmail.com>
2020-08-12 15:45:05 -0500

    Explicacion arrow fun

---
+++ Intersemestrales 2020-2/claseMiercoles/arrowfun.js
@@ -0,0 +1,20 @@
+class Persona {
+  constructor(nombre) {
+    this.nombre = nombre
+  }
+  funNormal() {
+    setTimeout(function () {
+      console.log('Funcion normal: ', this.nombre)
+    }, 1000)
+  }
+  funArrow() {
+    setTimeout(() => {
+      console.log('Funcion flecha: ', this.nombre)
+    }, 1000)
+  }
+}
+
+const persona = new Persona('Armando')
+console.log(persona.nombre)
+persona.funNormal()
+persona.funArrow()
